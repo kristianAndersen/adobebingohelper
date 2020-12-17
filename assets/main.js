fc064bbc03a5d824c15e404ad294fca84b51e19b
kristian andersen <krilleandersen@gmail.com>
2020-12-17 14:37:38 +0100

    Add files via upload
    Index-html-css

---
+++ assets/main.js
@@ -0,0 +1,46 @@
+let input = document.getElementById("input-box");
+
+
+input.addEventListener("keypress", function(e) {
+    e.stopPropagation()
+    if (e.key === 'Enter') {
+       
+        let inputValue = document.getElementById("input-box").value;
+     bp1(inputValue)
+
+    }
+    
+});
+
+
+function bp1(inputValue){
+  
+   let  x = document.getElementById("b1").rows.length;
+      
+   
+   console.log(x)
+   
+    
+    for(i=0;i<x;i++){
+        let tr1 = document.getElementsByTagName("tr")[i];
+        let tdl1 = tr1.getElementsByTagName("td").length;
+        
+       
+        
+        for(j=0;j<tdl1;j++){
+            let td1 = tr1.getElementsByTagName("td")[j];
+            console.log(td1.innerHTML+'=='+inputValue)
+            if(td1.innerHTML===inputValue){
+             
+              td1.className += " yes";
+           
+            }else{
+                console.log("nope")
+            }
+        
+        }
+    }
+
+    
+    
+    }
